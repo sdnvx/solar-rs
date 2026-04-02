b0fc6534c78ac28ad0754b4703784deb37731fef
Dmitry Sednev <dmitry@sednev.ru>
2026-04-03 01:14:18 +0300

    Add objects data reading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::collections::BTreeMap;
-use std::path::Path;
+use std::fs::File;
+use std::io::Read;
+use std::path::{Path, PathBuf};
 use std::process::ExitCode;
+ use std::sync::{Mutex, MutexGuard, OnceLock};
 
 use clap::Parser;
+use serde::Deserialize;
 
 /// Solar system simulator
 #[derive(Parser, Debug)]
@@ -13,18 +17,24 @@ struct Args {
     data: String
 }
 
+#[derive(Debug, Deserialize)]
 enum CelestialType {
     STAR,
     PLANET,
     MOON
 }
 
+#[derive(Debug, Deserialize)]
+struct CelestialSet {
+    #[serde(flatten)]
+    objects: BTreeMap<String, CelestialBody>
+}
+
+#[derive(Debug, Deserialize)]
 struct CelestialBody {
     name: String,
     kind: CelestialType
 }
-
-static mut OBJECTS: BTreeMap::<String, CelestialBody> = BTreeMap::new();
 
 fn main() -> ExitCode {
     let args = Args::parse();
@@ -33,32 +43,61 @@ fn main() -> ExitCode {
     println!("Copyright (c) 2026, Dmitry Sednev <dmitry@sednev.ru>");
     println!();
 
-    let data_dir = match Path::new(&args.data).canonicalize() {
-        Ok(dir) => {
-            if !dir.is_dir() {
-                println!("Not a directory: {:?}", dir);
-                return ExitCode::FAILURE;
-            }
+    let data_dir = get_data_dir(&args.data);
+    println!("Reading data from {}...", data_dir.display());
 
-            dir
-        }
-        Err(msg) => {
-            println!("Invalid path: {}: {}", args.data, msg);
-            return ExitCode::FAILURE;
-        }
-    };
-
-    println!("Reading data from {:?}...", data_dir);
     for entry in std::fs::read_dir(&data_dir).unwrap() {
         match entry {
             Ok(path) => {
-                println!("# {}", path.path().display());
+                read_data(&path.path());
             }
             Err(msg) => {
-                println!("Error: {}", msg);
+                panic!("Error: {}", msg);
             }
         }
     }
 
     return ExitCode::SUCCESS;
+}
+
+fn get_data_dir(path: &String) -> PathBuf {
+    let data_dir = match Path::new(path).canonicalize() {
+        Ok(dir) => {
+            if !dir.is_dir() {
+                panic!("Not a directory: {:?}", dir);
+            }
+
+            dir
+        }
+        Err(msg) => {
+            panic!("Invalid path: {}: {}", path, msg);
+        }
+    };
+
+    data_dir
+}
+
+fn get_object_map() -> MutexGuard<'static, BTreeMap<String, CelestialBody>> {
+    static MAP: OnceLock<Mutex<BTreeMap<String, CelestialBody>>> = OnceLock::new();
+
+    MAP.get_or_init(|| Mutex::new(BTreeMap::new()))
+        .lock()
+        .expect("Let's hope the lock isn't poisoned")
+}
+
+fn read_data(path: &Path) {
+    println!("# {}", path.display());
+
+    let mut file = File::open(path).unwrap();
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    let mut map = get_object_map();
+    let data: CelestialSet = toml::from_str(&contents).unwrap();
+
+    for (key, entry) in data.objects.into_iter() {
+        println!("... {:?}", entry.name);
+        map.insert(key, entry);
+    }
 }
